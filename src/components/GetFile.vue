108eb4dc05786c62dfb984f0b2e9e69507a5333d
Michael Eo <meo482@UoA.auckland.ac.nz>
2020-07-28 11:03:44 +1200

    'endpoins;

--- src/components/GetFile.vue
+++ src/components/GetFile.vue
@@ -35,7 +35,6 @@ export default {
             }
 
             var json = JSON.stringify(requestContent)
-            alert(json)
             $.post('http://127.0.0.1:5000/upload', json)
 
             $.ajax ({
